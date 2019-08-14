7a2f5ff0f8a7b34a60a303087a4c2f5cc202d999
Brian Musyoki <Musyokibr@gmail.com>
2019-08-14 12:20:09 +0300

    [Feature

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template,flash,redirect,url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ddd13bc2073648c445cad7dabca3ea42'
 
 posts = [
     {
@@ -35,6 +39,27 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='ABout Bruh!')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data } !', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@andela.com' and form.password.data == 'admin':
+            flash('you have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful please check username and password', 'danger')
+    return render_template('login.html', title='LogIn', form=form)
 
 
 if __name__ == '__main__':
