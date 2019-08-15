87f32e3bfd6f940de2b7f14125e449b1d336905f
Brian Musyoki <Musyokibr@gmail.com>
2019-08-15 15:37:33 +0300

    [Feature

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -17,9 +19,9 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('That username is taken please choose another username')
 
     def validate_email(self, email):
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('That email is already in use,cick signin to login')
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already in use,cick signin to login')
 
 
 class LoginForm(FlaskForm):
@@ -28,3 +30,21 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
 
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=15)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('update Profile Pic', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken please choose another username')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is already in use,cick signin to login')
